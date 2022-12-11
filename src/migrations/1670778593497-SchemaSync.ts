e6ec4403ff9f99d715c8877a4f0e8197ff10229b
Steve Larsen <slarsen-3@att.net>
2022-12-11 11:14:08 -0600

    setup migrations

---
+++ src/migrations/1670778593497-SchemaSync.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class SchemaSync1670778593497 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "coffee" ADD "description" character varying`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "coffee" DROP COLUMN "description"`);
+  }
+}
