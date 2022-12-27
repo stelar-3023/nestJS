6ba3d7e15a928c04f9798cda6fb9bc2ca9070534
Steve Larsen <slarsen-3@att.net>
2022-12-27 05:01:18 -0600

    added coffee rating

---
+++ src/coffee-rating/coffee-rating.module.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { CoffeesModule } from 'src/coffees/coffees.module';
+import { CoffeeRatingService } from './coffee-rating.service';
+
+@Module({
+  imports: [CoffeesModule],
+  providers: [CoffeeRatingService]
+})
+export class CoffeeRatingModule {}
