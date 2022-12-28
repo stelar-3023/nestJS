97db99ba2a5568cd9b41dce1d11f8a9fa55e4ef8
Steve Larsen <slarsen-3@att.net>
2022-12-28 13:11:02 -0600

    update prettier formatting

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Request } from 'express';
